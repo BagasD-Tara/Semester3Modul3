4d99dd057dd17b5ba870d5460b83f5e7c2b30350
dragonlava <bagasdewantara120@gmail.com>
2025-10-20 21:44:29 +0700

    commit

---
+++ src/Modul3/Codelab/Codelab1.java
@@ -0,0 +1,22 @@
+package Modul3.Codelab;
+
+import java.util.Scanner;
+
+public class Codelab1 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        try{
+            System.out.print("Masukkan usia Anda: ");
+            int age = scan.nextInt();
+
+            if (age <= 0 || age >= 120){
+                throw new Exception("Usia tidak valid! Usia harus antara 1 hingga 119.");
+            }
+
+            System.out.println("Usia valid: " + age);
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
